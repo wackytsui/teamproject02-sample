b944c9dc1ae4922a4947f3a1716976bbc63163e2
wackytsui <allan.altavano@gmail.com>
2024-04-04 20:59:46 +0800

    api

--- src/js/sample-api.js
+++ src/js/sample-api.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'https://www.themoviedb.org/api';
+export const BASE_URL = 'https://api.themoviedb.org/3/discover/movie';
 const API_KEY = 'af468b47558a0483ee60828cb8f72c98';
 
 export const options = {
